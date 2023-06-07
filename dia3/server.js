854d64203ed9b518f730f47c9a974eb7c22d84dc
thomaseade <eadethomas95@gmail.com>
2023-06-07 12:22:16 +0200

    reto1 dia3

---
+++ dia3/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use((req, res, next) => {
+  console.log("Petición recibida del cliente");
+  next();
+});
+
+
+app.use((req, res, next) => {
+  console.log(`URL: ${req.url}`);
+  console.log(`Método: ${req.method}`);
+  console.log(`User-Agent: ${req.get("User-Agent")}`);
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  res.status(200).json({ ok: true, message: "Recibido!" });
+});
+
+
+app.get('/bye', (req, res) => {
+  res.status(200).json({ ok: true, message: "Adios!" });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
