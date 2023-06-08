da8bc39873a47a973dfea51b6cfea9fd0148df21
thomaseade <eadethomas95@gmail.com>
2023-06-08 13:16:23 +0200

    reto 2 dia3

--- dia3/src/app.js
+++ dia3/src/app.js
@@ -1,11 +1,84 @@
+const express = require('express');
 const cors = require('cors');
+const Book = require('./models/book');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 app.use(cors());
 
+let book = new Book(1, 1, "Harry Potter", "Fiction", "J.K. Rowling", 19.99, "harry_potter.jpg");
 
+app.get('/book', (req, res) => {
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ message: 'No book found' });
+  }
+});
 
-const bookRouter = require('./routers/book.routers');
-app.use(bookRouter);
+app.post('/book', (req, res) => {
+  const { id_book, id_user, title, type, author, price, photo } = req.body;
 
+  if (!book) {
+    book = new Book(id_book, id_user, title, type, author, price, photo);
+    res.status(201).json({ message: 'Book created successfully' });
+  } else {
+    res.status(400).json({ message: 'Book already exists' });
+  }
+});
 
+app.put('/book', (req, res) => {
+  const { id_book, id_user, title, type, author, price, photo } = req.body;
 
-module.exports = app;
+  if (book) {
+    const updatedFields = {}; // Objeto para almacenar los campos actualizados
+
+    if (id_book !== undefined) {
+      book.id_book = id_book;
+      updatedFields.id_book = id_book;
+    }
+    if (id_user !== undefined) {
+      book.id_user = id_user;
+      updatedFields.id_user = id_user;
+    }
+    if (title !== undefined) {
+      book.title = title;
+      updatedFields.title = title;
+    }
+    if (type !== undefined) {
+      book.type = type;
+      updatedFields.type = type;
+    }
+    if (author !== undefined) {
+      book.author = author;
+      updatedFields.author = author;
+    }
+    if (price !== undefined) {
+      book.price = price;
+      updatedFields.price = price;
+    }
+    if (photo !== undefined) {
+      book.photo = photo;
+      updatedFields.photo = photo;
+    }
+
+    res.json({ message: 'Book updated successfully', updatedFields });
+  } else {
+    res.status(404).json({ message: 'No book found' });
+  }
+});
+
+app.delete('/book', (req, res) => {
+  if (book) {
+    book = null;
+    res.json({ message: 'Book deleted successfully' });
+  } else {
+    res.status(404).json({ message: 'No book found' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
