7cb453c9d3a7eebbba9c2e50b2277ef2ee22360f
thomaseade <eadethomas95@gmail.com>
2023-06-13 10:28:27 +0200

    arreglos reto3 id

--- dia3/API/routers/books.routers.js
+++ dia3/API/routers/books.routers.js
@@ -7,12 +7,12 @@ const booksController = require('../controller/books.controller');
 
 router.get('/books', booksController.getBooks);
 
-router.get('/books/:id', booksController.getBookById);
+router.get('/books', booksController.getBookById);
 
 router.post('/books', booksController.addBook);
 
-router.put('/books/:id', booksController.updateBook);
+router.put('/books', booksController.updateBook);
 
-router.delete('/books/:id', booksController.deleteBook);
+router.delete('/books', booksController.deleteBook);
 
 module.exports = router;
